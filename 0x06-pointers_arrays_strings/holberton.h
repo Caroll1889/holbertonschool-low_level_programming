aaee93ccc3e916343859a2f51e13daf502770284
Caroll1889 <diahancaroll@hotmail.com>
2019-07-04 16:57:44 +0000

    holberton

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,5 @@
+#ifndef holberton_h
+#define holberton_h
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -20,3 +22,12 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+#endif
