9f408d917e8fabbf83e416ffe6567894490f2f96
Caroll1889 <diahancaroll@hotmail.com>
2019-07-04 17:31:41 +0000

    world

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* *_strcat - concatenates two strings
+*@dest: pointer
+*@src: pointer
+*@n: number of bytes
+*Return: dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int b, k;
+
+	for (b = 0; dest[b] != '\0'; b++)
+	;
+	for (k = 0; src[k] != '\0' && k < n; k++)
+	{
+		dest[k + b] = src[k];
+	}
+	return (dest);
+}
