e649d7a8a05c5a7c0f09d6804fa6725efc9cacf2
Caroll1889 <diahancaroll@hotmail.com>
2019-07-16 20:33:35 +0000

    str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *_strdup - return a pointer to a new string
+*@str:string
+*Return: copy
+**/
+char *_strdup(char *str)
+{
+	unsigned int a, b;
+	char *copy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		copy = malloc(a * sizeof(str));
+	}
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+
+	b = 0;
+	while (b < a)
+	{
+		copy[b] = str[b];
+		b++;
+	}
+	return (copy);
+}
