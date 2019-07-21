34c1a7584c878a212eed231c4d2adb41dab590b0
Caroll1889 <diahancaroll@hotmail.com>
2019-07-21 01:54:11 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*print_alphabet - print the alphabet
+*Return: 0
+**/
+
+void print_alphabet(void)
+{
+	int h;
+
+	for (h = 'a'; h <= 'z'; h++)
+	{
+		_putchar(h);
+	}
+	_putchar('\n');
+
+}
