abe365af2d65a91fca3f220199ff0252ea74c0db
Caroll1889 <diahancaroll@hotmail.com>
2019-07-22 23:46:26 +0000

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef __FILE__
+#define __file__
+#endif
+
+/**
+* main - print the name of the file
+*Return: 0
+**/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
