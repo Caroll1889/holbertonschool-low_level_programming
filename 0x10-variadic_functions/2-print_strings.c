bc465f2b1fb6efb31d0af29634b272550a1f1b31
Caroll1889 <diahancaroll@hotmail.com>
2019-07-26 20:17:01 +0000

    printsprings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+*print_strings - print the strings
+*@n: numbers of arguments
+*@separator: string between strings
+*Return: nothing
+**/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int ld;
+
+	char *js;
+
+	va_list stn;
+
+	va_start(stn, n);
+
+	for (ld = 0; ld < n; ld++)
+	{
+		js = va_arg(stn, char *);
+
+		if (js == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", js);
+		}
+		if (separator != NULL && ld < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(stn);
+	printf("\n");
+}
