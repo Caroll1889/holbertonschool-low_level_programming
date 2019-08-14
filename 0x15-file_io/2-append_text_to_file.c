25e814a35ff02379134e21079ea259085cd461ca
Caroll1889 <diahancaroll@hotmail.com>
2019-08-14 04:21:36 +0000

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	of = open(filename, O_RDWR | O_APPEND);
+	of = open(filename, O_WRONLY | O_APPEND);
 
 	if (of == -1)
 	{
