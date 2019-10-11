77132cc8e90c42a84a88d6749f79178f7c54b3ac
Caroll1889 <diahancaroll@hotmail.com>
2019-10-11 20:27:26 +0000

    fixed requirement

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  * *hash_table_create - creates a hash table
  *@size: size of the array
  *Return: new_table
-**/
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
