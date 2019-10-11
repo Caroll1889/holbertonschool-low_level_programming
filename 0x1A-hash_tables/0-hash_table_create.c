4a001a3bc895d5ad9514a92bb1ac499f6f9611ba
Caroll1889 <diahancaroll@hotmail.com>
2019-10-11 20:21:35 +0000

    fixed Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,22 +10,22 @@
 **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *new_table;
+	hash_table_t *new_table;
 
-        if (size == 0)
-            return (NULL);
+	if (size == 0)
+		return (NULL);
 
-        new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 
-        if (new_table == NULL)
-                return (NULL);
+	if (new_table == NULL)
+		return (NULL);
 
-        new_table->size = size;
+	new_table->size = size;
 
-        new_table->array = calloc(size, sizeof(hash_node_t*));
+	new_table->array = calloc(size, sizeof(hash_node_t *));
 
-        if (new_table->array == NULL)
-                return (NULL);
-        
-        return (new_table);
+	if (new_table->array == NULL)
+	return (NULL);
+
+	return (new_table);
 }
