b82919e584c09e5d27f0e31ca0e3718b205c43ff
Caroll1889 <diahancaroll@hotmail.com>
2019-10-19 03:28:41 +0000

    print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+
+/**
+* hash_table_print - print a hash table
+* @ht: hash table
+* Return: Nothing
+**/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx, i = 0;
+	hash_node_t *temp;
+	
+	if (ht == NULL)
+	{
+		exit(1);
+	}
+
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+        	temp = ht->array[idx];
+		while (temp)
+		{
+			if (i)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			i++;
+		}
+	}
+	printf("}\n");
+}
+
