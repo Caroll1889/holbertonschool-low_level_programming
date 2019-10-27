bf584b1a9b14de316389f97dd2ac41c5c54bfd0b
Caroll1889 <diahancaroll@hotmail.com>
2019-10-27 22:55:13 +0000

    Selection sort

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+* selection_sort - sort an array using Selection Sort 
+* @array: array
+* @size: size
+* Return: nothing
+**/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t d, c, min;
+	int tmp;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (d = 0; d < size - 1; d++)
+	{
+		min = d;
+		for (c = d + 1; c < size; c++)
+		{
+			if (array[c] < array[min])
+			{
+				min = c;
+			}
+		}
+		tmp = array[d];
+		array[d] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+	}
+} 
