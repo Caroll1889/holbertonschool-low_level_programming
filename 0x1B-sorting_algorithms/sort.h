f2f4c6954ed53a1ce6d1aa81cfb35a248f2c08a3
Caroll1889 <diahancaroll@hotmail.com>
2019-10-29 20:57:19 +0000

    fixed betty

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -23,6 +23,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void check(listint_t *tem, listint_t **list);
-int _partition (int *array, int low, int high, size_t size);
+int _partition(int *array, int low, int high, size_t size);
 void _quicksort(int *array, int low, int high, size_t size);
 #endif /* _SORT_H_ */
