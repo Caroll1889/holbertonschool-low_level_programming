a4dc47e4c08336ac1d44e9416d7f8eb5a224396f
Caroll1889 <diahancaroll@hotmail.com>
2019-10-29 17:19:34 +0000

    insertion sort

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+/**
+* *inserton_sort_list - sorted a double linked list using insertion sort
+*@list: double linked list
+*Return: Nothing
+**/
+
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *h_move, *idx;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	h_move = *list;
+	idx = *list;
+	temp = *list;
+	while (h_move != NULL)
+	{
+		if (h_move == idx)
+			h_move = h_move->next;
+		else if (h_move != NULL && h_move->n < h_move->prev->n)
+		{
+			temp = h_move;
+			h_move = h_move->next;
+			temp->prev->next = temp->next;
+			if (h_move)
+				temp->next->prev = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next = temp->prev->next;
+			temp->prev->next = temp;
+			temp->next->prev = temp;
+			print_list(*list);
+			check(temp, list);
+		}
+		else
+		{
+			h_move = h_move->next;
+		}
+	}
+}
+
+void check(listint_t *temp, listint_t **list)
+{
+	while (temp->prev != NULL)
+	{
+		if (temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			temp->next->prev = temp->prev;
+			if (temp->prev->prev == NULL)
+			{
+				*list = temp;
+				temp->next = temp->prev;
+				temp->prev = NULL;
+			}
+			else
+			{
+				temp->prev = temp->prev->prev;
+				temp->next = temp->prev->next;
+				temp->prev->next = temp;
+			}
+			temp->next->prev = temp;
+			print_list(*list); 
+		}
+		else
+			temp = temp->prev;
+	}
+}
+
