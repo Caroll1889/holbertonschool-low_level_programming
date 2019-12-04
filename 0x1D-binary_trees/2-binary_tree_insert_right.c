f7bc02bde4c881d2e45bde82333438c85445d135
Caroll1889 <diahancaroll@hotmail.com>
2019-12-04 20:09:13 +0000

    conditionals

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -12,12 +12,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *n_node;
 	binary_tree_t *temp;
 
-	n_node = binary_tree_node(parent, value);
-
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+
+	n_node = binary_tree_node(parent, value);
 
 	if (parent->right != NULL)
 	{
